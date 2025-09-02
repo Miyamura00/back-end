30bfe41a94dee10ac2a99c67b1e33dedefe4c46e
Miyamura00 <iammartin00@gmail.com>
2025-09-02 11:03:20 +0800

    Update AWS region in DynamoDB configuration, downgrade Express to 4.18.2, and add path-to-regexp dependency. Update various packages in package-lock.json for compatibility and security improvements.

--- config/dynamodb.js
+++ config/dynamodb.js
@@ -2,7 +2,7 @@ const {DynamoDBClient, CreateTableCommand, KeyType, BillingMode} = require ('@aw
 const {DynamoDBDocumentClient} = require('@aws-sdk/lib-dynamodb')
 
 const client = new DynamoDBClient({
-    region: process.env.AWS_REGION || 'ap-northeast-2',
+    region: process.env.AWS_REGION || 'ap-southeast-2',
     credentials: {
         accessKeyId: process.env.AWS_ACCESS_KEY_ID,
         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
